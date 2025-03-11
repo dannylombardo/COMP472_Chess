c0adc3e42aef05265b8621c3d8ddc379046570ee
AhmadO <ahmadyaobeid@gmail.com>
2025-03-11 18:14:45 -0400

    comitting most recent works from dev

--- src/MiniChessSkeletonCode.py
+++ src/MiniChessSkeletonCode.py
@@ -9,11 +9,11 @@ WhiteMoveCounter = 0
 BlackMoveCounter = 0
 TIME_LIMIT = 5  # Time limit in seconds for the AI to make a move
 
+
 class MiniChess:
     def __init__(self):
         self.current_game_state = self.init_board()
         self.move_counter = 0
-
 
     """
     Initialize the board
@@ -26,19 +26,19 @@ class MiniChess:
 
     def init_board(self):
         state = {
-                "board": 
+            "board":
                 [['bK', 'bQ', 'bB', 'bN', '.'],
-                ['.', '.', 'bp', 'bp', '.'],
-                ['.', '.', '.', '.', '.'],
-                ['.', 'wp', 'wp', '.', '.'],
-                ['.', 'wN', 'wB', 'wQ', 'wK']],
-                "turn": 'white',
-                }
+                 ['.', '.', 'bp', 'bp', '.'],
+                 ['.', '.', '.', '.', '.'],
+                 ['.', 'wp', 'wp', '.', '.'],
+                 ['.', 'wN', 'wB', 'wQ', 'wK']],
+            "turn": 'white',
+        }
         return state
 
     """
     Prints the board
-    
+
     Args:
         - game_state: Dictionary representing the current game state
     Returns:
@@ -69,12 +69,12 @@ class MiniChess:
         end_row, end_col = end
         piece = game_state["board"][start_row][start_col]
 
-        if piece == '.': # No piece to move
+        if piece == '.':  # No piece to move
             return False
 
         piece_color = 'white' if piece[0] == 'w' else 'black'
 
-        if piece_color != game_state["turn"]: # Check if it's the correct turn
+        if piece_color != game_state["turn"]:  # Check if it's the correct turn
             return False
 
         valid_moves = self.valid_moves(game_state)
@@ -89,8 +89,6 @@ class MiniChess:
     Returns:
         - valid moves:   list | A list of nested tuples corresponding to valid moves [((start_row, start_col),(end_row, end_col)),((start_row, start_col),(end_row, end_col))]
     """
-
-    
 
     def valid_moves(self, game_state):
         moves = []
@@ -196,28 +194,27 @@ class MiniChess:
 
         # ----------- Scenario 1: Moves a piece from one location to the other as well as when a piece is removed from the game(for general cases ONLY) -----------
 
-        moveCodeLetter = 'A'                    # Initialized to random letter (used for the logger notation)
-        moveCodeNumber = '1'                    # Initialized to random number (used for the logger notation)
-        eliminated = False                      # Initialized to a piece not being eliminated
-        pawnToQueen = False                     # Initialized switch that checks if pawn has become queen
+        moveCodeLetter = 'A'  # Initialized to random letter (used for the logger notation)
+        moveCodeNumber = '1'  # Initialized to random number (used for the logger notation)
+        eliminated = False  # Initialized to a piece not being eliminated
+        pawnToQueen = False  # Initialized switch that checks if pawn has become queen
 
-        start_row, start_col = start            # the x position of the coordinate goes to start_row and the y position goes start_col
-        end_row, end_col = end                  # this step isn't needed but just for easier readability
-        
-        pieceEliminated = game_state["board"][0][4]                     # random spot (just to initialize)
+        start_row, start_col = start  # the x position of the coordinate goes to start_row and the y position goes start_col
+        end_row, end_col = end  # this step isn't needed but just for easier readability
 
-        piece = game_state["board"][start_row][start_col]               # copies the piece that is being moved
-        game_state["board"][start_row][start_col] = '.'                 # replaces that initial spot with a '.'
+        pieceEliminated = game_state["board"][0][4]  # random spot (just to initialize)
+
+        piece = game_state["board"][start_row][start_col]  # copies the piece that is being moved
+        game_state["board"][start_row][start_col] = '.'  # replaces that initial spot with a '.'
 
         if game_state["board"][end_row][end_col] != '.':
-            pieceEliminated = game_state["board"][end_row][end_col]     # keeps track of the eliminated piece
-            eliminated = True                                           # turns on the switch (that piece has been eliminated in this turn)
+            pieceEliminated = game_state["board"][end_row][end_col]  # keeps track of the eliminated piece
+            eliminated = True  # turns on the switch (that piece has been eliminated in this turn)
 
-        game_state["board"][end_row][end_col] = piece                   # updates the piece at the end location
-
+        game_state["board"][end_row][end_col] = piece  # updates the piece at the end location
 
         if end_row == 0:
-            moveCodeNumber = '5'              # Initializing the code for the number to be used in the logger (since they dont have the same numbering and lettering)
+            moveCodeNumber = '5'  # Initializing the code for the number to be used in the logger (since they dont have the same numbering and lettering)
         elif end_row == 1:
             moveCodeNumber = '4'
         elif end_row == 2:
@@ -228,7 +225,7 @@ class MiniChess:
             moveCodeNumber = '1'
 
         if end_col == 0:
-            moveCodeLetter = 'A'              # Initializing the code for the letter to be used in the logger (since they dont have the same numbering and lettering)
+            moveCodeLetter = 'A'  # Initializing the code for the letter to be used in the logger (since they dont have the same numbering and lettering)
         elif end_col == 1:
             moveCodeLetter = 'B'
         elif end_col == 2:
@@ -237,66 +234,62 @@ class MiniChess:
             moveCodeLetter = 'D'
         elif end_col == 4:
             moveCodeLetter = 'E'
-        
 
-        if game_state["turn"] == "white":                               # switches the turn of the player
+        if game_state["turn"] == "white":  # switches the turn of the player
             game_state["turn"] = "black"
         else:
             game_state["turn"] = "white"
-        
 
         # ----------- Scenario 2: If a white pawn moves to the last row, it becomes a white queen -----------
 
         if game_state["board"][0][0] == 'wp':
-            game_state["board"][0][0] = 'wQ'                # becomes a queen (for white)
+            game_state["board"][0][0] = 'wQ'  # becomes a queen (for white)
             pawnToQueen = True
 
         elif game_state["board"][0][1] == 'wp':
-            game_state["board"][0][1] = 'wQ'                # becomes a queen (for white)
+            game_state["board"][0][1] = 'wQ'  # becomes a queen (for white)
             pawnToQueen = True
 
         elif game_state["board"][0][2] == 'wp':
-            game_state["board"][0][2] = 'wQ'                # becomes a queen (for white)
+            game_state["board"][0][2] = 'wQ'  # becomes a queen (for white)
             pawnToQueen = True
 
         elif game_state["board"][0][3] == 'wp':
-            game_state["board"][0][3] = 'wQ'                # becomes a queen (for white)
+            game_state["board"][0][3] = 'wQ'  # becomes a queen (for white)
             pawnToQueen = True
 
         elif game_state["board"][0][4] == 'wp':
-            game_state["board"][0][4] = 'wQ'                # becomes a queen (for white)
+            game_state["board"][0][4] = 'wQ'  # becomes a queen (for white)
             pawnToQueen = True
 
-        
         # ----------- Scenario 3: If a black pawn moves to the last row, it becomes a black queen -----------
 
-
         if game_state["board"][4][0] == 'bp':
-            game_state["board"][4][0] = 'bQ'                # becomes a queen (for black)
+            game_state["board"][4][0] = 'bQ'  # becomes a queen (for black)
             pawnToQueen = True
 
         elif game_state["board"][4][1] == 'bp':
-            game_state["board"][4][1] = 'bQ'                # becomes a queen (for black)
+            game_state["board"][4][1] = 'bQ'  # becomes a queen (for black)
             pawnToQueen = True
 
         elif game_state["board"][4][2] == 'bp':
-            game_state["board"][4][2] = 'bQ'                # becomes a queen (for black)
+            game_state["board"][4][2] = 'bQ'  # becomes a queen (for black)
             pawnToQueen = True
 
         elif game_state["board"][4][3] == 'bp':
-            game_state["board"][4][3] = 'bQ'                # becomes a queen (for black)
+            game_state["board"][4][3] = 'bQ'  # becomes a queen (for black)
             pawnToQueen = True
 
         elif game_state["board"][4][4] == 'bp':
-            game_state["board"][4][4] = 'bQ'                # becomes a queen (for black)
+            game_state["board"][4][4] = 'bQ'  # becomes a queen (for black)
             pawnToQueen = True
-
 
         # ----------- Scenario 4: If the white king is eliminated -----------
 
         if pieceEliminated == 'wK':
             with open("COMP472_Project.txt", "a") as f:
-                f.write(piece + ' has moved to ' + (moveCodeLetter + moveCodeNumber) + ' and the white king has been eliminated ' + '\n' + 'GAME OVER: BLACK WINS \n')
+                f.write(piece + ' has moved to ' + (
+                            moveCodeLetter + moveCodeNumber) + ' and the white king has been eliminated ' + '\n' + 'GAME OVER: BLACK WINS \n')
             print("Black wins!" + str(BlackMoveCounter) + " moves")
             sys.exit(0)
 
@@ -304,27 +297,28 @@ class MiniChess:
 
         if pieceEliminated == 'bK':
             with open("COMP472_Project.txt", "a") as f:
-                f.write(piece + ' has moved to ' + (moveCodeLetter + moveCodeNumber) + ' and the black king has been eliminated ' + '\n' + 'GAME OVER: WHITE WINS \n')
+                f.write(piece + ' has moved to ' + (
+                            moveCodeLetter + moveCodeNumber) + ' and the black king has been eliminated ' + '\n' + 'GAME OVER: WHITE WINS \n')
             print("White wins!" + str(WhiteMoveCounter) + " moves")
             sys.exit(0)
 
-
         # ----------- Game / Move logger (Tracks the moves in the game) -----------
 
-        
         if log_move:
             if eliminated == True:
                 with open("COMP472_Project.txt", "a") as f:
-                    f.write(piece + ' has moved to ' + (moveCodeLetter + moveCodeNumber) + ' and ' + pieceEliminated + ' is now eliminated from the game \n')
+                    f.write(piece + ' has moved to ' + (
+                                moveCodeLetter + moveCodeNumber) + ' and ' + pieceEliminated + ' is now eliminated from the game \n')
             elif pawnToQueen == False:
                 with open("COMP472_Project.txt", "a") as f:
                     f.write(piece + ' has moved to ' + (moveCodeLetter + moveCodeNumber) + '\n')
             else:
                 with open("COMP472_Project.txt", "a") as f:
-                    f.write(piece + ' has moved to ' + (moveCodeLetter + moveCodeNumber) + ' and it is now become a queen' + '\n')
+                    f.write(piece + ' has moved to ' + (
+                                moveCodeLetter + moveCodeNumber) + ' and it is now become a queen' + '\n')
 
-        pawnToQueen = False                                 # reset the pawnToQueen to false so that it can be checked for in the future moves
-        eliminated = False                                  # reset the eliminated to false so that it can check for the future moves
+        pawnToQueen = False  # reset the pawnToQueen to false so that it can be checked for in the future moves
+        eliminated = False  # reset the eliminated to false so that it can check for the future moves
 
         if captured_piece != '.':
             self.move_counter = 0
@@ -356,12 +350,12 @@ class MiniChess:
                 f.write("Black Move Counter: " + str(BlackMoveCounter) + "\n")
 
         return game_state
-        
+
     def captured_piece(self, game_state, end):
         end_row, end_col = end
         piece = game_state["board"][end_row][end_col]
         return piece if piece != '.' else '.'
-    
+
     # Check if the king is on the board
     def king_exists(self, game_state):
         for row in game_state["board"]:
@@ -377,11 +371,12 @@ class MiniChess:
     Returns:
         - (start, end)  tuple | the move to perform ((start_row, start_col),(end_row, end_col))
     """
+
     def parse_input(self, move):
         try:
             start, end = move.split()
-            start = (5-int(start[1]), ord(start[0].upper()) - ord('A'))
-            end = (5-int(end[1]), ord(end[0].upper()) - ord('A'))
+            start = (5 - int(start[1]), ord(start[0].upper()) - ord('A'))
+            end = (5 - int(end[1]), ord(end[0].upper()) - ord('A'))
             return (start, end)
         except:
             return None
@@ -394,6 +389,7 @@ class MiniChess:
     Returns:
         - None
     """
+
     def play(self):
         print("Welcome to Mini Chess!")
 
@@ -458,10 +454,10 @@ class MiniChess:
             'wp': 1, 'wB': 3, 'wN': 3, 'wQ': 9, 'wK': 999,
             'bp': 1, 'bB': 3, 'bN': 3, 'bQ': 9, 'bK': 999
         }
-        
+
         white_score = 0
         black_score = 0
-        
+
         for row in game_state["board"]:
             for piece in row:
                 if piece in piece_values:
@@ -469,8 +465,9 @@ class MiniChess:
                         white_score += piece_values[piece]
                     else:
                         black_score += piece_values[piece]
-        
+
         return white_score - black_score
+
 
 if __name__ == "__main__":
     game = MiniChess()
